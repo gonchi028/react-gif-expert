843436d6ceb1a9ab8f65f536e929b086ab3fe785
Gonzalo Quispe <gonchi.028@gmail.com>
2022-07-27 20:19:59 -0400

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newInputValue = inputValue.trim();
+    if (newInputValue.length < 1) return;
+    // setCategories(cat => [inputValue, ...cat]);
+    onNewCategory(newInputValue);
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
